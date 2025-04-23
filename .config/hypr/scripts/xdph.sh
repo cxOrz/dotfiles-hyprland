246216435324ef51427fb6cb01c4835ec25e612c
cxOrz <cxOrz@outlook.com>
2025-04-23 19:19:39 +0800

    update hyprland config

--- .config/hypr/scripts/xdph.sh
+++ .config/hypr/scripts/xdph.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-sleep 4
+sleep 1
 killall -e xdg-desktop-portal-hyprland
-killall -e xdg-desktop-portal-wlr
 killall xdg-desktop-portal
 /usr/lib/xdg-desktop-portal-hyprland &
-sleep 4
+sleep 2
 /usr/lib/xdg-desktop-portal &
